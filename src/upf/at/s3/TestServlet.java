50290c5a73d3245bbb6e5209e74c5dd52f78cfa6
Yihan2255 <yihan.jin01@estudiant.upf.edu>
2026-02-04 09:24:04 +0100

    e

---
+++ src/upf/at/s3/TestServlet.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class TestServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+        // Requirement: Use an HTTP Header
+        String userAgent = request.getHeader("User-Agent"); 
+        
+        // Get the data from the HTML Form
+        String name = request.getParameter("username"); 
+        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<html><body>");
+        out.println("<h1>Hello, " + (name != null ? name : "Guest") + "!</h1>");
+        out.println("<p>Your browser is: " + userAgent + "</p>");
+        out.println("</body></html>");
+    }
+}
